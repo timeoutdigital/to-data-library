acb419cf4845b0928dc7039cbed4955ee3f17258
Gzim Hasani <hasani.gzim.1996@gmail.com>
2023-08-31 11:44:29 +0100

    unit tests

---
+++ tests/test_gs.py
@@ -0,0 +1,48 @@
+import os
+import unittest
+from unittest import mock
+from unittest.mock import Mock
+
+from google.cloud import storage
+
+from tests.setup import setup
+from to_data_library.data.gs import Client
+
+# def setUpModule():
+#     setup.create_bucket()
+
+
+def tearDownModule():
+    setup.cleanup()
+
+
+class TestGS(unittest.TestCase):
+
+    # def __init__(self, *args, **kwargs):
+    #     super(TestGS, self).__init__(*args, **kwargs)
+    #     self.setup = setup
+    #     self.gs_uri = f'gs://{self.setup.bucket_name}/sample.csv'
+
+    @mock.patch('to_data_library.data.gs.storage')
+    def test_download(self, mock_storage):
+        test_client = Client(project='fake_project')
+
+        test_client.download(gs_uri='/fake_uri.csv')
+        self.assertTrue(os.path.exists('fake_uri.csv'))
+
+        test_client.download(gs_uri='/fake_uri', destination_file_name='fake_des.csv')
+        self.assertTrue(os.path.exists('fake_des.csv'))
+
+    @mock.patch('to_data_library.data.gs.storage')
+    def test_upload(self, mock_storage):
+        mock_client = mock_storage.Client.return_value
+        mock_bucket = Mock()
+        mock_client.bucket.return_value = mock_bucket
+        
+        test_client = Client(project='fake_project')
+        
+        test_client.upload('tests/data/sample.csv', 'fake_bucket')
+        mock_bucket.blob.assert_called_with('sample.csv')
+        
+        test_client.upload('tests/data/sample.csv', 'fake_bucket', 'new_name')
+        mock_bucket.blob.assert_called_with('new_name')
--- to_data_library/data/gs.py
+++ to_data_library/data/gs.py
@@ -13,31 +13,36 @@ class Client:
         self.project = project
         self.storage_client = storage.Client(project=self.project)
 
-    def download(self, gs_uri, destination_file_name):
+    def download(self, gs_uri, destination_file_name=None):
         """Download from Google Storage to local.
 
         Args:
             gs_uri (str):  The Google Storage uri. For example: ``gs://my_bucket_name/my_filename``.
             destination_file_name (str):  The destination file name. For example: ``/some_path/some_file_name``.
+            If not provided, destination_file_name will be name of file in GCS.
         """
+        if not destination_file_name:
+            destination_file_name = gs_uri.split('/')[-1]
 
         with open(destination_file_name, 'wb') as file_obj:
             self.storage_client.download_blob_to_file(gs_uri, file_obj)
 
-    def upload(self, source_file_name, bucket_name, blob_name):
+    def upload(self, source_file_name, bucket_name, blob_name=None):
         """Upload from local to Google Storage.
 
         Args:
             source_file_name (str):  The source file name.
             bucket_name (str):  The Google Storage bucket name.
-            blob_name (str): The destination file name in the bucket
+            blob_name (str): The destination file name in the bucket, if not provided source file name will be used.
         """
-
+        if not blob_name:
+            blob_name = source_file_name.split('/')[-1]
+            
         bucket = self.storage_client.bucket(bucket_name)
         blob = bucket.blob(blob_name)
         blob.upload_from_filename(source_file_name)
 
-    def create(self, bucket_name):
+    def create_bucket(self, bucket_name):
         """create a bucket in Google Storage.
 
         Args:
