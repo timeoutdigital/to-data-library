b0be8789495324a4e989a88a444576c4f23497eb
Jennifer Holmes <jennifer.holmes@timeout.com>
2025-07-16 16:47:12 +0100

    DSS-3165 (#25)
    For this ticket:
    https://timeoutgroup.atlassian.net/browse/DSS-3165?atlOrigin=eyJpIjoiNzc1OGI1OWJiODljNDA1YzlkMGVkMjlkMDRlMGQyMzgiLCJwIjoiaiJ9
    I have looked into updating to-data-library to include a process for
    uploading files from s3 to big query, however, when looking into the
    library I found the method already exists as well as a method to
    transfer files from s3 to gcs.
    
    I tested using the methods in an existing splash da and found the way
    partitioning was defined was not compatible with our code so I have made
    changes to correct this and keep the code similar to the gs_to_bq method
    (which I have used before).
    
    I also have added some schema validation and tests. I would be
    interested to know if you think we need more schema validation and if
    some of the tests are too repetitive.
    Also do you think the schema structure is sufficient? I have tried to
    look at the data for the ads reporting but not sure I'm looking in the
    right place.

--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -105,3 +105,239 @@ def test_gs_to_bq(self, mock_default, mock_loadjobconfig,
         res = client._get_keys_in_s3_bucket(mock_aws_session, 'fake_bucket_name', 'fake_prefix_name')
 
         self.assertEqual(res, [])
+
+    def test_gs_to_bq_schema_validation(self):
+        client = transfer.Client('fake_project_name')
+        valid_schema = [('field1', 'STRING'), ('field2', 'INTEGER')]
+        with patch('google.cloud.bigquery.Client'), \
+             patch('google.cloud.bigquery.DatasetReference'), \
+             patch('google.cloud.bigquery.TableReference'), \
+             patch('google.cloud.bigquery.LoadJobConfig'):
+            # Should not raise
+            client.gs_to_bq(
+                gs_uris="gs://bucket/file.csv",
+                table="p.d.t",
+                write_preference="truncate",
+                schema=valid_schema
+            )
+            # Invalid schema: wrong type
+            with self.assertRaises(ValueError):
+                client.gs_to_bq(
+                    gs_uris="gs://bucket/file.csv",
+                    table="p.d.t",
+                    write_preference="truncate",
+                    schema=[('field1', 'NOTATYPE')]
+                )
+
+    def test_ftp_to_bq_schema_validation(self):
+        client = transfer.Client('fake_project_name')
+        valid_schema = [('field1', 'STRING')]
+        with patch('to_data_library.data.ftp.Client') as mock_ftp, \
+             patch('to_data_library.data.bq.Client') as mock_bq:
+            mock_ftp.return_value.download_file.return_value = '/tmp/file.csv'
+            mock_bq.return_value.upload_table.return_value = None
+            # Should not raise
+            client.ftp_to_bq(
+                ftp_connection_string='user:pass@host:21',
+                ftp_filepath='/file.csv',
+                bq_table='p.d.t',
+                write_preference='truncate',
+                bq_table_schema=valid_schema
+            )
+            # Invalid schema
+            with self.assertRaises(ValueError):
+                client.ftp_to_bq(
+                    ftp_connection_string='user:pass@host:21',
+                    ftp_filepath='/file.csv',
+                    bq_table='p.d.t',
+                    write_preference='truncate',
+                    bq_table_schema=[('field1', 'NOTATYPE')]
+                )
+
+    def test_gs_parquet_to_bq_schema_validation(self):
+        client = transfer.Client('fake_project_name')
+        valid_schema = [('field1', 'STRING')]
+        with patch('google.cloud.bigquery.Client'), \
+             patch('google.cloud.bigquery.DatasetReference'), \
+             patch('google.cloud.bigquery.TableReference'), \
+             patch('google.cloud.bigquery.LoadJobConfig'):
+            # Should not raise
+            client.gs_parquet_to_bq(
+                gs_uris="gs://bucket/file.parquet",
+                table="p.d.t",
+                write_preference="truncate",
+                schema=valid_schema
+            )
+            # Invalid schema: wrong type
+            with self.assertRaises(ValueError):
+                client.gs_parquet_to_bq(
+                    gs_uris="gs://bucket/file.parquet",
+                    table="p.d.t",
+                    write_preference="truncate",
+                    schema=[('field1', 'NOTATYPE')]
+                )
+
+    def test_s3_to_bq_schema_validation(self):
+        client = transfer.Client('fake_project_name')
+        valid_schema = [('field1', 'STRING')]
+        with patch('to_data_library.data.s3.Client') as mock_s3, \
+             patch('to_data_library.data.bq.Client') as mock_bq:
+            mock_s3.return_value.download.return_value = '/tmp/file.csv'
+            mock_bq.return_value.create_dataset.return_value = None
+            mock_bq.return_value.load_table_from_uris.return_value = None
+            # Should not raise
+            client.s3_to_bq(
+                aws_session=Mock(),
+                bucket_name='bucket',
+                object_name='file.csv',
+                bq_table='p.d.t',
+                write_preference='truncate',
+                schema=valid_schema
+            )
+            # Invalid schema
+            with self.assertRaises(ValueError):
+                client.s3_to_bq(
+                    aws_session=Mock(),
+                    bucket_name='bucket',
+                    object_name='file.csv',
+                    bq_table='p.d.t',
+                    write_preference='truncate',
+                    schema=[('field1', 'NOTATYPE')]
+                )
+
+    def test_s3_to_bq_partitioning_and_config(self):
+        client = transfer.Client('fake_project_name')
+        valid_schema = [('field1', 'STRING')]
+        with patch('to_data_library.data.s3.Client') as mock_s3, \
+             patch('to_data_library.data.bq.Client') as mock_bq, \
+             patch('os.remove') as mock_remove, \
+             patch('os.path.exists', return_value=True):
+            mock_s3.return_value.download.return_value = '/tmp/file.csv'
+            mock_bq.return_value.create_dataset.return_value = None
+            mock_bq.return_value.load_table_from_uris.return_value = None
+
+            # Test with partition_date and partition_field, custom delimiter, source_format, max_bad_records
+            client.s3_to_bq(
+                aws_session=Mock(),
+                bucket_name='bucket',
+                object_name='file.csv',
+                bq_table='p.d.t',
+                write_preference='truncate',
+                schema=valid_schema,
+                partition_date='20240101',
+                partition_field='field1',
+                source_format='CSV',
+                separator='|',
+                max_bad_records=5
+            )
+
+            # Check that os.remove was called for cleanup
+            mock_remove.assert_called_once_with('/tmp/file.csv')
+            # Check that load_table_from_uris was called (table_id should have $20240101)
+            args, kwargs = mock_bq.return_value.load_table_from_uris.call_args
+            self.assertIn('$20240101', str(args[1].table_id))
+            # Check job_config attributes
+            job_config = kwargs['job_config']
+            self.assertEqual(job_config.source_format, 'CSV')
+            self.assertEqual(job_config.field_delimiter, '|')
+            self.assertEqual(job_config.max_bad_records, 5)
+
+
+class TestTransferEdgeCases(unittest.TestCase):
+    def setUp(self):
+        self.client = transfer.Client('fake_project')
+
+    def test_gs_to_bq_invalid_source_format(self):
+        with patch('google.cloud.bigquery.Client'), \
+             patch('google.cloud.bigquery.DatasetReference'), \
+             patch('google.cloud.bigquery.TableReference'), \
+             patch('google.cloud.bigquery.LoadJobConfig'), \
+             patch.object(transfer.logs.client.logger, 'error') as mock_error:
+            with self.assertRaises(SystemExit):
+                self.client.gs_to_bq(
+                    gs_uris='gs://bucket/file.unknown',
+                    table='p.d.t',
+                    write_preference='truncate',
+                    source_format='NOT_A_FORMAT'
+                )
+            mock_error.assert_called_with('Invalid SourceFormat entered: NOT_A_FORMAT')
+
+    def test_gs_to_bq_partition_field_without_date(self):
+        with patch('google.cloud.bigquery.Client'), \
+             patch('google.cloud.bigquery.DatasetReference'), \
+             patch('google.cloud.bigquery.TableReference'), \
+             patch('google.cloud.bigquery.LoadJobConfig'), \
+             patch.object(transfer.logs.client.logger, 'error') as mock_error:
+            with self.assertRaises(SystemExit):
+                self.client.gs_to_bq(
+                    gs_uris='gs://bucket/file.csv',
+                    table='p.d.t',
+                    write_preference='truncate',
+                    partition_field='field1'
+                )
+            mock_error.assert_called()
+
+    def test_s3_to_bq_invalid_source_format(self):
+        with patch('to_data_library.data.s3.Client'), \
+             patch('to_data_library.data.bq.Client'), \
+             patch('os.path.exists', return_value=True), \
+             patch('os.remove'), \
+             patch.object(transfer.logs.client.logger, 'error') as mock_error:
+            with self.assertRaises(SystemExit):
+                self.client.s3_to_bq(
+                    aws_session=Mock(),
+                    bucket_name='bucket',
+                    object_name='file.csv',
+                    bq_table='p.d.t',
+                    write_preference='truncate',
+                    source_format='NOT_A_FORMAT'
+                )
+            mock_error.assert_called_with('Invalid SourceFormat entered: NOT_A_FORMAT')
+
+    def test_s3_to_bq_partition_field_without_date(self):
+        with patch('to_data_library.data.s3.Client'), \
+             patch('to_data_library.data.bq.Client'), \
+             patch('os.path.exists', return_value=True), \
+             patch('os.remove'), \
+             patch.object(transfer.logs.client.logger, 'error') as mock_error:
+            with self.assertRaises(SystemExit):
+                self.client.s3_to_bq(
+                    aws_session=Mock(),
+                    bucket_name='bucket',
+                    object_name='file.csv',
+                    bq_table='p.d.t',
+                    write_preference='truncate',
+                    partition_field='field1'
+                )
+            mock_error.assert_called()
+
+    def test_s3_to_gs_cleanup_and_logging(self):
+        with patch('to_data_library.data.s3.Client') as mock_s3, \
+             patch('to_data_library.data.gs.Client') as mock_gs, \
+             patch('os.remove') as mock_remove, \
+             patch('os.path.exists', return_value=True), \
+             patch.object(transfer.logs.client.logger, 'info') as mock_info:
+            mock_s3.return_value.download.return_value = '/tmp/file.csv'
+            mock_gs.return_value.upload.return_value = None
+            # Simulate one file found
+            with patch.object(self.client, '_get_keys_in_s3_bucket', return_value=['file.csv']):
+                self.client.s3_to_gs(
+                    aws_session=Mock(),
+                    s3_bucket_name='bucket',
+                    s3_object_name='file.csv',
+                    gs_bucket_name='gs-bucket',
+                )
+                mock_remove.assert_called_once_with('/tmp/file.csv')
+                mock_info.assert_any_call('Deleted local file /tmp/file.csv')
+
+    def test_get_keys_in_s3_bucket_empty_and_missing_contents(self):
+        # Empty pages
+        mock_aws_session = Mock()
+        mock_aws_session.client.return_value.get_paginator.return_value.paginate.return_value = []
+        res = self.client._get_keys_in_s3_bucket(mock_aws_session, 'bucket', 'prefix')
+        self.assertEqual(res, [])
+        # Page with no 'Contents'
+        mock_aws_session.client.return_value.get_paginator.return_value.paginate.return_value = [{'NoContents': True}]
+        with patch('builtins.print'):
+            res = self.client._get_keys_in_s3_bucket(mock_aws_session, 'bucket', 'prefix')
+        self.assertEqual(res, [])
